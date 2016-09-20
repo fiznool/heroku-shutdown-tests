fc2b2d0dcee10d724c96a52606f1b31dbe96fec3
Tom Spencer <fiznool@gmail.com>
2016-09-20 11:55:43 +0100

    Added ticker

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 'use strict';
 
-// Ensure app stays alive
-process.stdin.resume();
-
 console.log('Started');
+
+let active = true;
 
 const shutdown = function() {
   console.log('Shutting down.');
-  process.exit(0);
+  active = false;
+  process.on('TICK', () => process.exit(0));
 };
 
 process.on('SIGINT', shutdown);
 process.on('SIGTERM', shutdown);
+
+const tick = function() {
+  setTimeout(() => {
+    console.log('tick');
+    process.emit('TICK');
+    if(active) {
+      tick();
+    }
+  }, 1000);
+
+};
+
+tick();
