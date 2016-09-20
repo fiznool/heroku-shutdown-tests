f85ac8eec3887f178bc59c324032f6de8d523a65
Tom Spencer <fiznool@gmail.com>
2016-09-20 12:08:11 +0100

    Web server

--- index.js
+++ index.js
@@ -33,3 +33,22 @@ const tick = function() {
 };
 
 tick();
+
+// Start a web server so that heroku is happy
+var http = require('http');
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('<!DOCTYPE html>');
+  response.write('<html>');
+  response.write('<head>');
+  response.write('<title>Hello World Page</title>');
+  response.write('</head>');
+  response.write('<body>');
+  response.write('Hello World!');
+  response.write('</body>');
+  response.write('</html>');
+  response.end();
+});
+
+server.listen(process.env.PORT || 8080);
+console.log('Server is listening');
